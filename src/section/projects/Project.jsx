37cce99b8074bce2436af7fe8c5c8b66eac552a7
sachin kumar <goputen215@gmail.com>
2025-01-16 14:21:54 +0530

    update

--- src/section/projects/Project.jsx
+++ src/section/projects/Project.jsx
@@ -1,13 +1,14 @@
 import ProjectCard from './card/ProjectCard'
 import viberr from '../../assets/mitlogo.png'
 import styles from './Project.module.css'
-import moviescreenshot from '../../assets/moviescreenshot.png'
+import bubbleSortImage from '../../assets/bubbleSort.png'
 function Project() {
   return (
 <section className={styles.container}>
   <h1 className={styles.projecttitle}>Project</h1>
   <div className={styles.projectContainer}>
    <ProjectCard viberr={viberr} h3="Paper Website" p="full-stack project ( currently working...)" link="http://mitpaperdown.rf.gd/MIT/login.php/"></ProjectCard>
+   <ProjectCard viberr={bubbleSortImage} h3="Bubble Sort " p="Visualization" link="https://sachin-kumar-2003.github.io/BubbleSort_Visualization/"></ProjectCard>
   </div>
 </section>
   )

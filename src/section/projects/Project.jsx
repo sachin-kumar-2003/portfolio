a63c64ad9d7bfcf424d568dac5fcce5b0abda542
sachin kumar <goputen215@gmail.com>
2025-01-16 14:28:08 +0530

    Update Project.jsx

--- src/section/projects/Project.jsx
+++ src/section/projects/Project.jsx
@@ -8,7 +8,7 @@ function Project() {
 <section className={styles.container}>
   <h1 className={styles.projecttitle}>Project</h1>
   <div className={styles.projectContainer}>
-   <ProjectCard viberr={viberr} h3="Paper Website" p="full-stack project ( currently working...)" link="http://mitpaperdown.rf.gd/MIT/login.php/"></ProjectCard>
+   <ProjectCard viberr={viberr} h3="Paper Website" p="(Currently working)" link="http://mitpaperdown.rf.gd/MIT/login.php/"></ProjectCard>
    <ProjectCard viberr={bubbleSortImage} h3="Bubble Sort " p="Visualization" link="https://sachin-kumar-2003.github.io/BubbleSort_Visualization/"></ProjectCard>
    <ProjectCard viberr={numberPuzzleImage} h3="Number Puzzle" p="Game" link="https://sachin-kumar-2003.github.io/NumberPuzzle/"></ProjectCard>
   </div>

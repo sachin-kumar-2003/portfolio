f5dad08f5843ed08a88ad8b1067fa87999ddbf85
sachin kumar <goputen215@gmail.com>
2025-01-16 14:31:50 +0530

    update

--- src/section/projects/Project.jsx
+++ src/section/projects/Project.jsx
@@ -3,6 +3,7 @@ import viberr from '../../assets/mitlogo.png'
 import styles from './Project.module.css'
 import bubbleSortImage from '../../assets/bubbleSort.png'
 import numberPuzzleImage from '../../assets/numPuzzle.png'
+import imdb from '../../assets/imdb.png'
 function Project() {
   return (
 <section className={styles.container}>
@@ -11,6 +12,7 @@ function Project() {
    <ProjectCard viberr={viberr} h3="Paper Website" p="(Currently working)" link="http://mitpaperdown.rf.gd/MIT/login.php/"></ProjectCard>
    <ProjectCard viberr={bubbleSortImage} h3="Bubble Sort " p="Visualization" link="https://sachin-kumar-2003.github.io/BubbleSort_Visualization/"></ProjectCard>
    <ProjectCard viberr={numberPuzzleImage} h3="Number Puzzle" p="Game" link="https://sachin-kumar-2003.github.io/NumberPuzzle/"></ProjectCard>
+   <ProjectCard viberr={imdb} h3="IMDB Clone" p="Website" link="https://imdb-clone-one-rho.vercel.app/"></ProjectCard>
   </div>
 </section>
   )

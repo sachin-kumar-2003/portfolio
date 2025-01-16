56dec19d720de47f1b4eee045c62256452ef3b58
sachin kumar <goputen215@gmail.com>
2025-01-16 14:26:36 +0530

    add

--- src/section/projects/Project.jsx
+++ src/section/projects/Project.jsx
@@ -2,6 +2,7 @@ import ProjectCard from './card/ProjectCard'
 import viberr from '../../assets/mitlogo.png'
 import styles from './Project.module.css'
 import bubbleSortImage from '../../assets/bubbleSort.png'
+import numberPuzzleImage from '../../assets/numPuzzle.png'
 function Project() {
   return (
 <section className={styles.container}>
@@ -9,6 +10,7 @@ function Project() {
   <div className={styles.projectContainer}>
    <ProjectCard viberr={viberr} h3="Paper Website" p="full-stack project ( currently working...)" link="http://mitpaperdown.rf.gd/MIT/login.php/"></ProjectCard>
    <ProjectCard viberr={bubbleSortImage} h3="Bubble Sort " p="Visualization" link="https://sachin-kumar-2003.github.io/BubbleSort_Visualization/"></ProjectCard>
+   <ProjectCard viberr={numberPuzzleImage} h3="Number Puzzle" p="Game" link="https://sachin-kumar-2003.github.io/NumberPuzzle/"></ProjectCard>
   </div>
 </section>
   )
